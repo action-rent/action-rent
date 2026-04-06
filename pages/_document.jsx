22684858a8fc28ceb6dfbd20eee1c636fbfb598f
Artem <vottema97@gmail.com>
2026-04-06 08:04:26 +0500

    fix bldk

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,5 @@
 // pages/_app.jsx
-import "../styles/App.css"; // твой основной CSS
+import "../styles/App.css"; // твой основной CSS  ee
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
